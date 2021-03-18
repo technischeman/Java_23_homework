bdd26b43d3f5f39f97b5a6d1cf1b51c783d11741
technischeman <79165779+technischeman@users.noreply.github.com>
2021-03-18 16:23:02 +0100

    Add files via upload

---
+++ java_homework23/src/cember.java
@@ -0,0 +1,19 @@
+public class cember {
+    static double π = 3.14;
+    private int yaricap_cember;
+
+    double cevre;
+    double alan;
+
+    public double cembercevre(double r) {
+        cevre = 2 * π * r;
+        return cevre;
+    }
+
+    public double alancember(double r) {
+        alan = π * r*r;
+        return alan;
+
+
+    }
+}
---
+++ java_homework23/src/main.java
@@ -0,0 +1,16 @@
+public class main {
+    public static void main(String[] args) {
+        cember cember1=new cember();
+        cember cember2=new cember();
+
+   double cember1_cevre=cember1.cembercevre(12);
+        System.out.println("birinci cember cevre: "+cember1_cevre);
+        System.out.println("birinci cember alan:"+cember1.alancember(12));
+      double cember2_cevre=cember2.cembercevre(15);
+        System.out.println("ikinci cember cevre:"+cember2_cevre);
+       double alan_cember2= cember2.alancember(15);
+        System.out.println("alan 2. cember:"+alan_cember2);
+
+
+    }
+}
